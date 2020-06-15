0c5ca166c05bb026847138e5e44c50f52073177a
FedQa_R <63184742+FedQa@users.noreply.github.com>
2020-06-15 13:46:14 +0500

    Add files via upload

---
+++ 4thTaskPrac/4thTaskPrac/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4thTaskPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите точность e, e>0");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            int i = 0;
+            double function = 1 / (Math.Pow(4, i) + Math.Pow(5, i + 2));
+            double sum = 0;
+            while(Math.Abs(function)>e)
+            {
+                sum = sum+function;
+                i++;
+                function = 1 / (Math.Pow(4, i) + Math.Pow(5, i + 2));
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
